4b2c519d8a8bac5b88013344e6132c94712d7162
Tony <tony@gmail.com>
2018-08-29 18:13:46 +0800

    完成遍历省市县三级列表的功能” df sdff
    git commit -m 完成遍历省市县三级列表的功能”

--- app/src/main/java/android/iroquoisweather/com/iroquoisweather/db/County.java
+++ app/src/main/java/android/iroquoisweather/com/iroquoisweather/db/County.java
@@ -2,7 +2,7 @@ package android.iroquoisweather.com.iroquoisweather.db;
 
 import org.litepal.crud.DataSupport;
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
     private String countryName;
     private String weatherId;
